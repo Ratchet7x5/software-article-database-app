3f3c73c079b50c63827540b02ad377b6545d0e98
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-06-20 11:28:43 +0530

    update ViewArticle page

--- client/src/pages/ViewAllArticles.js
+++ client/src/pages/ViewAllArticles.js
@@ -6,15 +6,15 @@ const ViewAllArticles = () => {
   const [data, getData] = useState([])
 
   useEffect(() => {
-      fetchData()
+    fetchData()
   }, [])
 
   const fetchData = () => {
-      getAllArticles()
-          .then((response) => {
-              console.log(response);
-              getData(response.data);
-          })
+    getAllArticles()
+        .then((response) => {
+            console.log(response);
+            getData(response.data);
+        })
   }
 
   return (
@@ -22,26 +22,26 @@ const ViewAllArticles = () => {
         <h2>Articles in database:</h2>
         <tbody>
           <tr>
-              <th>Title</th>
-              <th>Authors</th>
-              <th>Source</th>
-              <th>Published Year</th>
-              <th>DOI</th>
-              <th>Claim</th>
-              <th>Evidence</th>
-              <th>SE Practice</th>
+            <th>Title</th>
+            <th>Authors</th>
+            <th>Source</th>
+            <th>Published Year</th>
+            <th>DOI</th>
+            <th>Claim</th>
+            <th>Evidence</th>
+            <th>SE Practice</th>
           </tr>
             {data.map((item, i) => (
-                <tr key={i}>
-                    <td>{item.title}</td>
-                    <td>{item.authors}</td>
-                    <td>{item.source}</td>
-                    <td>{item.pubyear}</td>
-                    <td>{item.doi}</td>
-                    <td>{item.claim}</td>
-                    <td>{item.evidence}</td>
-                    <td>{item.sepractice}</td>
-                </tr>
+              <tr key={i}>
+                <td>{item.title}</td>
+                <td>{item.authors}</td>
+                <td>{item.source}</td>
+                <td>{item.pubyear}</td>
+                <td>{item.doi}</td>
+                <td>{item.claim}</td>
+                <td>{item.evidence}</td>
+                <td>{item.sepractice}</td>
+              </tr>
             ))}
           </tbody>
       </>
