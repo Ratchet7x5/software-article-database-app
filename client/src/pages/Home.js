d74f115c6107f6d3db55e0cee497c8c5c96296ae
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2022-07-04 13:43:42 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     return (
       <div>
         <h2>Welcome to the SPEED Application!</h2>
-        <p>This is the Home Page of the SPEED Application. This is a work in progress!</p>
+        <p>This is the Home Page of the SPEED Application. <br>To use this application, you must submit your article information in the "Submit an Article" page. Fill out the required details, and click submit. <br>To view articles in the database, simply go to the "View All Articles" page. You can see all the submitted articles in the database.</p>
       </div>
     );
   }
