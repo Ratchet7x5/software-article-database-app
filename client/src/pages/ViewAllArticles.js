59080b2b9f7741c2181d7c7a5f027b3339060c4a
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-09-04 12:23:07 +1200

    add file info in ViewAllArticles.js, adjust code style

--- client/src/pages/ViewAllArticles.js
+++ client/src/pages/ViewAllArticles.js
@@ -1,50 +1,52 @@
-import React, {useEffect, useState} from "react";
+/**
+ * This page is where all the data from MongoDB will be loaded into a table and displayed.
+ */
+
+import React, { useEffect, useState } from "react";
 import { getAllArticles } from "../services/articleservice";
 
-
 const ViewAllArticles = () => {
-  const [data, getData] = useState([])
+  const [data, getData] = useState([]);
 
   useEffect(() => {
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   const fetchData = () => {
-    getAllArticles()
-        .then((response) => {
-            console.log(response);
-            getData(response.data);
-        })
-  }
+    getAllArticles().then((response) => {
+      console.log(response);
+      getData(response.data);
+    });
+  };
 
   return (
-      <>
-        <h2>Articles in database:</h2>
-        <tbody>
-          <tr>
-            <th>Title</th>
-            <th>Authors</th>
-            <th>Source</th>
-            <th>Published Year</th>
-            <th>DOI</th>
-            <th>Claim</th>
-            <th>Evidence</th>
-            <th>SE Practice</th>
+    <>
+      <h2>Articles in database:</h2>
+      <tbody>
+        <tr>
+          <th>Title</th>
+          <th>Authors</th>
+          <th>Source</th>
+          <th>Published Year</th>
+          <th>DOI</th>
+          <th>Claim</th>
+          <th>Evidence</th>
+          <th>SE Practice</th>
+        </tr>
+        {data.map((item, i) => (
+          <tr key={i}>
+            <td>{item.title}</td>
+            <td>{item.authors}</td>
+            <td>{item.source}</td>
+            <td>{item.pubyear}</td>
+            <td>{item.doi}</td>
+            <td>{item.claim}</td>
+            <td>{item.evidence}</td>
+            <td>{item.sepractice}</td>
           </tr>
-            {data.map((item, i) => (
-              <tr key={i}>
-                <td>{item.title}</td>
-                <td>{item.authors}</td>
-                <td>{item.source}</td>
-                <td>{item.pubyear}</td>
-                <td>{item.doi}</td>
-                <td>{item.claim}</td>
-                <td>{item.evidence}</td>
-                <td>{item.sepractice}</td>
-              </tr>
-            ))}
-          </tbody>
-      </>
+        ))}
+      </tbody>
+    </>
   );
 };
 
